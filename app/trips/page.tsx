1454a11b46eb1d363747e4254097d5a8bf81119a
kennycodin <kenechukwuokoh30@gmail.com>
2023-12-19 11:26:59 +0100

    Deploy

---
+++ app/trips/page.tsx
@@ -0,0 +1,43 @@
+import ClientOnly from '../components/ClientOnly';
+import EmptyState from '../components/EmptyState';
+import TripsClient from './TripsClient';
+
+import getCurrentUser from '../actions/getCurrentUser';
+import getReservations from '../actions/getReserations';
+
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you haven't reserved any trips"
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <div>
+      <ClientOnly>
+        <TripsClient reservations={reservations} currentUser={currentUser} />
+      </ClientOnly>
+    </div>
+  );
+};
+
+export default TripsPage;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { hostname } = require('os');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
