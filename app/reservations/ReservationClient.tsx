0e7cac39c4c98281d5819fb330b17db043414b62
kennycodin <kenechukwuokoh30@gmail.com>
2023-12-19 11:57:40 +0100

    Fixed ListingCard data error

--- app/reservations/ReservationClient.tsx
+++ app/reservations/ReservationClient.tsx
@@ -45,7 +45,7 @@ const ReservationClient: React.FC<ReservationsClientProps> = ({
     <Container>
       <Heading title="Reservations" subtitle="Bookings on your properties" />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {reservations.map((reservation) => (
+        {reservations.map((reservation: any) => (
           <ListingCard
             key={reservation.id}
             data={reservation.listing}
--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -48,7 +48,7 @@ const TripsClient: React.FC<TripsClientProps> = ({
         subtitle="Where you've been and where you're going"
       />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {reservations.map((reservation) => (
+        {reservations.map((reservation: any) => (
           <ListingCard
             key={reservation.id}
             data={reservation.listing}
